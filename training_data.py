f2592292ec889117ea972f162209f8279fad0285
Jay Dunkelberger <jaydunk@gmail.com>
2015-09-21 20:54:49 -0700

    added some salary data

--- training_data.py
+++ training_data.py
@@ -28,7 +28,7 @@ def make_total_game_data(seasons=range(2004,2015), weeks=range(1,18),pages=[0,1]
 	for season in seasons:
 		for week in weeks:
 			for page in pages:
-				df = read_player_stats.read_stats(season, week=week,page=page,pos=pos, scoring='PPR')
+				df = read_player_stats.read_stats(season, week=week,page=page,pos=pos, scoring='FD')
 				if total_df is None:
 					total_df = df
 				else:
@@ -129,6 +129,40 @@ def train_svm_model(training_df, verbose=False):
 			for epsilon_test in epsilon_list:
 				svm_model = svm.SVR(kernel='rbf', C=c_test, gamma=gamma_test, epsilon=epsilon_test)
 				scores = cross_validation.cross_val_score(svm_model, X_scaled, y_train, cv=5, scoring='mean_absolute_error')
+				mean_score = np.mean(scores)
+				if verbose:
+					print('params: ' + str(svm_model.get_params()))
+					print(mean_score)
+				if abs(mean_score) < abs(best_score):
+					best_score = mean_score
+					best_params = svm_model.get_params()
+
+	print('***Best Params***')
+	print(best_params)
+	print('Score:' + str(best_score))
+
+	#return a trained SVM with the best parameters we found
+	ret_svm = svm.SVR()
+	ret_svm.set_params(**best_params)
+	ret_svm.fit(X_scaled, y_train)
+	return ret_svm
+
+def train_gbg_svm_model(X_train, y_train, verbose=False):
+	#train SVM
+	X_scaled = preprocessing.scale(X_train)
+
+	best_score = 9999
+	best_params = {}
+
+	#grid search to optimize parameters of SVM
+	C_list = np.logspace(-5,2,num=11)
+	gamma_list = np.logspace(-3,1,num=11)
+	epsilon_list = [.1]
+	for c_test in C_list:
+		for gamma_test in gamma_list:
+			for epsilon_test in epsilon_list:
+				svm_model = svm.SVR(kernel='rbf', C=c_test, gamma=gamma_test, epsilon=epsilon_test)
+				scores = cross_validation.cross_val_score(svm_model, X_scaled, y_train, cv=5)
 				mean_score = np.mean(scores)
 				if verbose:
 					print('params: ' + str(svm_model.get_params()))
